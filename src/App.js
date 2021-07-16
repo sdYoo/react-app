11e4f56eb42a7ec3a2db0fa829ce9e5fa5f7a5ab
root <megaboy1129@naver.com>
2021-07-16 23:27:37 +0900

    first commit

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import Morpheuslogo from './img/morpheus-logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={Morpheuslogo} className="App-logo" alt="logo" />
+        <p>
+          Morpheus PoC CI/CD Test Page!
+        </p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
