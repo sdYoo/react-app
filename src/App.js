4395a9ae329c83d4e41f3399bf52f73cda158cd0
root <megaboy1129@naver.com>
2021-07-18 19:44:14 +0900

    test01

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
         <img src={Morpheuslogo} className="App-logo" alt="logo" />
         <p>
           Morpheus PoC CI/CD Test Page!
-          (2021.07.18-0011)
+          (2021.07.18-00112)
         </p>
       </header>
     </div>
